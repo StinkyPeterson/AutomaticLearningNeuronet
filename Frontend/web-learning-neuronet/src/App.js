c382ae3921590be87fa8fdb7f8f2788c5964b0fb
Artem <asbudin@edu.hse.ru>
2024-02-16 20:28:12 +0500

    инициализация веб проекта

---
+++ Frontend/web-learning-neuronet/src/App.js
@@ -0,0 +1,13 @@
+import 'devextreme/dist/css/dx.light.css';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'devextreme-react';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
